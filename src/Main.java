d94635dd2f02ad1fc1d2fea344f47f2afe19de67
DevVitorSilva <vitor.silva001080@gmail.com>
2024-01-30 12:07:54 -0300

    receiving the value that each vector will have

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,8 @@ public class Main {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-
+        System.out.print("How many values will each vector have? ");
+        int quantity = sc.nextInt();
         sc.close();
     }
 }
