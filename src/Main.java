50308d064da5fd33bf8981d6f9c113e74ed1d224
DevVitorSilva <vitor.silva001080@gmail.com>
2024-01-30 12:20:23 -0300

    declaring the first and second vector. Generating the third vector with the sum of the first and second vector

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,24 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.print("How many values will each vector have? ");
         int quantity = sc.nextInt();
+        int[] firstVector = new int[quantity];
+        int[] secondVector = new int[quantity];
+        System.out.println("Enter the value of vector A:");
+        for(int i = 0; i < quantity; i++){
+            System.out.print("Enter value here: ");
+            firstVector[i] = sc.nextInt();
+        }
+        System.out.println("Enter the value of vector B:");
+        for(int i = 0; i < quantity; i++){
+            System.out.print("Enter value here: ");
+            secondVector[i] = sc.nextInt();
+        }
+        int[] thirdVector = new int[quantity];
+        System.out.println("Value of third vector: ");
+        for(int i = 0; i < quantity; i++){
+            thirdVector[i] = firstVector[i] + secondVector[i];
+            System.out.println(thirdVector[i]);
+        }
         sc.close();
     }
 }
